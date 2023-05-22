7b5de3ce1e906eae3275dcd5f3fcfaab4fb5a825
Pedro Reis <dropecosta@gmail.com>
2023-05-22 09:03:11 +0100

    update files

--- components/Repository.tsx
+++ components/Repository.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import { GoStar as StarIcon, GoRepoForked as ForkIcon } from "react-icons/go";
 import { RepositoryInterface } from "../types/index";
-import FavoriteButton from "./FavoriteButton";
 
 interface RepositoryListProps {
   data: RepositoryInterface;
@@ -10,8 +9,6 @@ interface RepositoryListProps {
 const RepositoryList: React.FC<RepositoryListProps> = ({ data }) => {
   return (<>
     <div className="carousel-item relative w-64 h-60 snap-start bg-zinc-800">
-    <div className="w-full flex justify-end"><FavoriteButton id={data?.id} /></div>
-      
       <a className="h-full w-full aspect-video block bg-origin-padding bg-left-top bg-cover bg-no-repeat z-0"></a>
       <a className="h-full w-full aspect-square block absolute top-0 left-0 transition-opacity duration-30 z-10">
         <h3 className="text-white text-2xl underline py-2 px-3 mx-auto">{data?.name}</h3>
